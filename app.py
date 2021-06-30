0d59376045d4630d9c79dc5da6646efceecdb7ee
HS419MADHU <86403245+HS419MADHU@users.noreply.github.com>
2021-06-30 20:33:47 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.title('CALCULATOR')
+a=st.text_input('enter first number')
+b=st.text_input('enter second number')
+select=st.selectbox("optons",("ADDITION","SUBTRACTION","MULTIPLICATION","DIVISION"))
+if select=="ADDITION":
+  calc=int(a)+int(b)
+if select=="SUBTRACTION":
+  calc=int(a)-int(b)
+if select=="MULTIPLICATION":
+  calc=int(a)*int(b)
+if select=="DIVISION":
+  calc=int(a)/int(b)
+if st.button("RESULT"):
+  st.title(f'result of {select} of {a} and {b} is {calc}')
